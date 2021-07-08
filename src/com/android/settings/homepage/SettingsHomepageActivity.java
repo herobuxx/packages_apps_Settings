DotOS/android_packages_apps_Settings

Dashboard: Redesign searchbar header
Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
Change-Id: I8055882b085a2f52cd14bc1150575c68180b4232

src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -84,11 +84,7 @@ public class SettingsHomepageActivity extends FragmentActivity {
     void setHomepageContainerPaddingTop() {
         final View view = this.findViewById(R.id.homepage_container);
 
-        final int searchBarHeight = getResources().getDimensionPixelSize(R.dimen.search_bar_height);
-        final int searchBarMargin = getResources().getDimensionPixelSize(R.dimen.search_bar_margin);
-
-        // The top padding is the height of action bar(48dp) + top/bottom margins(16dp)
-        final int paddingTop = searchBarHeight + searchBarMargin * 2;
+        final int paddingTop = getResources().getDimensionPixelSize(R.dimen.dot_settings_bar_height);
         view.setPadding(0 /* left */, paddingTop, 0 /* right */, 0 /* bottom */);
 
         // Prevent inner RecyclerView gets focus and invokes scrolling.

herobuxx/packages_apps_Settings

Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -84,11 +84,7 @@ public class SettingsHomepageActivity extends FragmentActivity {
     void setHomepageContainerPaddingTop() {
         final View view = this.findViewById(R.id.homepage_container);
 
-        final int searchBarHeight = getResources().getDimensionPixelSize(R.dimen.search_bar_height);
-        final int searchBarMargin = getResources().getDimensionPixelSize(R.dimen.search_bar_margin);
-
-        // The top padding is the height of action bar(48dp) + top/bottom margins(16dp)
-        final int paddingTop = searchBarHeight + searchBarMargin * 2;
+        final int paddingTop = getResources().getDimensionPixelSize(R.dimen.conquer_settings_bar_height);
         view.setPadding(0 /* left */, paddingTop, 0 /* right */, 0 /* bottom */);
 
         // Prevent inner RecyclerView gets focus and invokes scrolling.